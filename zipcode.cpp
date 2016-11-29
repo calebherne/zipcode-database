9a3be791bf473046d576f66b59fa16087fb75143
Caleb Herne <cherne@umail.iu.edu>
2016-11-29 15:51:34 -0500

    added all function

--- zipcode.cpp
+++ zipcode.cpp
@@ -1,3 +1,30 @@
 #include <iostream>
 
 
+
+zipcode::zipcode::zipcode();
+zipcode::zipcode(std::string);
+zipcode::~zipcode();
+    int getzip();
+    std::string gettype();
+    std::string getmaincity();
+    std::string getsecondcity();
+    std::string getstate();
+    std::string getcouty();
+    std::string gettimezone();
+    std::string getareacode();
+    int getlatitude();
+    int getlongitude();
+    int getpopulation();
+    void setzip(int);
+    void settype(std::string);
+    void setmaincity(std::string);
+    void setsecondcity(std::string);
+    void setstate(std::string);
+    void setcounty(std::string);
+    void settimezone(std::string);
+    void setareacode(std::string);
+    void setlatitude(int);
+    void setlongitude(int);
+    void setpopulation(int);
+
--- zipcode.h
+++ zipcode.h
@@ -5,18 +5,50 @@
 class zipcode{
 
   protected:
-    int zip;
-    std::string type;
+    int* zip;
+    std::string* type;
     //no reason to have a decommisioned cat b/c none 
     //have been decommisioned
-    std::string maincity; 
-    std::string secondcity;
+    std::string* maincity; 
+    std::string* secondcity;
     //dont know what unacceptable city means so I didnt include it
-    std::string state;
-    std::string county;
-    std::string timezone;
-    std::string areacode;
+    std::string* state;
+    std::string* county;
+    std::string* timezone;
+    std::string* areacode;
     //didnt use country or world region b/c they are all America
-    std::string latitiude;
-    std::string longitude;
-    int population;
+    int* latitiude;
+    int* longitude;
+    int* population;
+
+  public:
+
+    zipcode();
+    zipcode(std::string);
+    ~zipcode();
+    int getzip();
+    std::string gettype();
+    std::string getmaincity();
+    std::string getsecondcity();
+    std::string getstate();
+    std::string getcouty();
+    std::string gettimezone();
+    std::string getareacode();
+    int getlatitude();
+    int getlongitude();
+    int getpopulation();
+    void setzip(int);
+    void settype(std::string);
+    void setmaincity(std::string);
+    void setsecondcity(std::string);
+    void setstate(std::string);
+    void setcounty(std::string);
+    void settimezone(std::string);
+    void setareacode(std::string);
+    void setlatitude(int);
+    void setlongitude(int);
+    void setpopulation(int);
+
+};
+
+#endif
